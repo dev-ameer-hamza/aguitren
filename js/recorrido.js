86772de3a825fcb68baa0bf9d3998f0adc2ce765
dev-ameer-hamza <ameer.hamza@ikasle.egibide.org>
2022-09-29 00:19:50 +0200

    mas animaciones

--- js/recorrido.js
+++ js/recorrido.js
@@ -5,6 +5,10 @@ let cerrarPuertas = document.getElementById("cerrar-puertas");
 let puertaIzq = document.getElementById("puerta-izq");
 let puertaDer = document.getElementById("puerta-der");
 
+let parar = document.getElementById("parar");
+let adelante = document.getElementById("adelante");
+let atras = document.getElementById("atras");
+
 campoVelocidad.addEventListener("input",function(e){
     console.log("se cambia la velocidad " + e.target.value );
 });
@@ -13,6 +17,10 @@ campoVelocidad.addEventListener("input",function(e){
 abrirPuertas.addEventListener('click',function(){
     puertaIzq.classList.add('abrir');
     puertaDer.classList.add('abrir');
+
+    puertaIzq.style.animation = "abrirpuertas 3s";
+    puertaDer.style.animation = "abrirpuertas 3s";
+
     abrirPuertas.style.display = "none";
     cerrarPuertas.style.display = "block";
 
@@ -22,9 +30,38 @@ cerrarPuertas.addEventListener("click",function(e){
     puertaIzq.classList.remove('abrir');
     puertaDer.classList.remove('abrir');
 
+    puertaIzq.style.animation = "cerrarpuertas 3s";
+    puertaDer.style.animation = "cerrarpuertas 3s";
+
     puertaIzq.classList.add('cerrar');
     puertaDer.classList.add('cerrar');
 
     abrirPuertas.style.display = "block";
     cerrarPuertas.style.display = "none";
+});
+
+
+parar.addEventListener("click",function(){
+    let imgTren  = document.getElementsByClassName('img-train');
+    console.log(imgTren[0].getBoundingClientRect());
+    imgTren[0].style.animationPlayState = "paused";
+});
+
+
+adelante.addEventListener("click",function(){
+    let imgTren  = document.getElementsByClassName('img-train');
+    imgTren[0].classList.add('remove-flip');
+    imgTren[0].classList.remove('flip-img');
+
+    imgTren[0].style.animationPlayState = "running";
+});
+
+atras.addEventListener("click",function(){
+    let imgTren  = document.getElementsByClassName('img-train');
+    // alert(imgTren[0].style.right);
+    imgTren[0].classList.add('flip-img');
+    imgTren[0].classList.remove('remove-flip');
+
+    // imgTren[0].classList.add('remove-animation');
+    imgTren[0].style.animation = "rideatras 10s linear 1s infinite";
 });
